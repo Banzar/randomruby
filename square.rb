6c4d2c559b9535f46c9e5c1eaf8f6b4a70c04ac4
David Ledford <dcledford@ymail.com>
2011-12-08 10:07:14 -0500

    Update square.rb

--- square.rb
+++ square.rb
@@ -1,10 +1,15 @@
+#!/usr/bin/env ruby
 #squareroot.rb
 #Get the squareroot of any number.
-puts "What number would you like the squareroot for?"
+begin
+print "What number would you like the squareroot for?: "
 number1 = gets.chomp.to_f
-if number1.to_f > 0
-numbsqrt = Math.sqrt(number1.to_f())
+if (number1 > 0)
+numbsqrt = Math.sqrt(number1)
 	puts "The squareroot of #{number1} is #{numbsqrt}"
 else
 print "This is not a valid number."
 end
+print "Would you like another squareroot? (Yes or no): "
+again = gets.chomp
+end until (again == "no" or again == "n")
